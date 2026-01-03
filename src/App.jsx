83b4590dc93a36c3da3489e6bbb0d36e9a947263
Ojaswini <ojaswini.volukula@gmail.com>
2026-01-03 20:53:15 +0530

    last mini reataurant managemnet app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import ProtectedRoute from "./routes/ProtectedRoute";
-import AddRestaurant from "./components/RestaurantCard";
+import AddRestaurant from "./pages/admin/AddRestaurant";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import UpdateRestaurant from "./pages/admin/UpdateRestaurant";
 import CustomerDashboard from "./customer/CustomerDashboard";
+import RestaurantCard from "./components/RestaurantCard";
 import {
   RestaurantContext,
   RestaurantProvider,
