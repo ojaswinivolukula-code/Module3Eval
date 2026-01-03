168aaa00f36f7a3da0765b783fc8e2db68998f53
Ojaswini <ojaswini.volukula@gmail.com>
2026-01-03 20:04:36 +0530

    admindashboard

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,15 +1,30 @@
-import { useContext } from "react"
-import { useNavigate } from "react-router-dom"
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
-
-
-
-function AdminDashboard(){
-const{restaurants,deleteRestaurant}=useContext(RestaurantContext)
-const navigate=useNavigate();
-
-
-
-
+function AdminDashboard() {
+  const { restaurants, deleteRestaurant } = useContext(RestaurantContext);
+  const navigate = useNavigate();
+  return (
+    <div>
+      <button onClick={() => navigate("admin/add")}>Add Restaurant</button>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill,250px)",
+          gap: "20",
+        }}
+      >
+        {restaurants.map((r) => (
+          <RestaurantCard
+            key={r.restaurantID}
+            data={r}
+            isAdmin
+            onDelete={() => deleteRestaurant(r.restaurantID)}
+            onUpdate={() => navigate(`/admin/update/${r.restaurantID}`)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
-export default AdminDashboard
+export default AdminDashboard;
